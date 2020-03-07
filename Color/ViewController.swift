fcf7d1024f80ee8190e4b6fd8421f039fd3462d1
BeisenovaElvira <47022749+BeisenovaElvira@users.noreply.github.com>
2020-03-07 18:22:37 +0600

    midtask

---
+++ Color/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Color
+//
+//  Created by Elvira Beisenova on 3/7/20.
+//  Copyright © 2020 Elvira Beisenova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func aa(_ sender: Any) {
+        
+    }
+    
+    @IBAction func bb(_ sender: Any) {
+    }
+    
+    @IBAction func cc(_ sender: Any) {
+    }
+    @IBAction func dd(_ sender: Any) {
+    }
+    @IBAction func red(_ sender: Any) {
+    }
+    @IBAction func blue(_ sender: Any) {
+    }
+}
+
