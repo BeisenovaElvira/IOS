95ccb0028bd1101aa7f66adaadb5d5f88f816f07
BeisenovaElvira <47022749+BeisenovaElvira@users.noreply.github.com>
2020-03-09 20:48:31 +0600

    lab2

---
+++ calculator1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator1
+//
+//  Created by Elvira Beisenova on 2/22/20.
+//  Copyright © 2020 Elvira Beisenova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
